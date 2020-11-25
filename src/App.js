a329feef23de04a49a7ef57a07d41eb41e56a0d1
Ramandeep Ramarayane <ramandeep.r49@gmail.com>
2020-11-25 17:24:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import Projects from './Components/Projects.js';
+import Skills from './Components/Skills.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Projects />
+      <Skills />
+    </div>
+  );
+}
+
+export default App;
